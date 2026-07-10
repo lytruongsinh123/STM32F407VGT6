43158083002f0351c4c3a9880e142f23b5f84697
lytruongsinh123 <diepduong607@gmail.com>
2026-07-10 10:09:22 +0700

    change

--- Drivers/Src/stm32f407xx_gpio_driver.c
+++ Drivers/Src/stm32f407xx_gpio_driver.c
@@ -162,6 +162,12 @@ void GPIO_Init(GPIO_Handle_t* pGPIOHandle)
             EXTI->FTSR |= (1 << pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber);
         }
         // 2. Configure the GPIO Port selection in SYSCFG_EXTICR
+        uint8_t temp1    = pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber / 4;
+        uint8_t temp2    = pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber % 4;
+        uint8_t portcode = GPIO_BASEADDR_TO_CODE(pGPIOHandle->pGPIOx);
+        SYSCFG_PCLK_EN();
+        SYSCFG->EXTICR[temp1] &= ~(0xF << (temp2 * 4));     // Clear the 4 bits of the pin to be configured
+        SYSCFG->EXTICR[temp1] |= (portcode << (temp2 * 4)); // Write the new port
         // 3. Enable the EXTI interrupt delivery using IMR
         EXTI->IMR |= (1 << pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber);
     }
